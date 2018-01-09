b4675851d1db6b10176d6f27bce1f362e27c2e48
Matt Bryson <mattbryson@gmail.com>
2018-01-09 12:40:13 +0000

    Update index.js
    updated comments

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,3 @@
-/**
- * Cloud Functions for Contract HQ Firebase app.
- *
- * Defines a custom API to handle user creation and validation as well as various firestore event handlers
- *
- */
-
 //Firebase App set up
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
